9dd4beaf38204ce2a968ee80e270a57c26c425af
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-09 10:34:15 +0200

    Update and rename test_erc20.py to test_erc20-SA.py

--- Tests/test_erc20.py
+++
@@ -1,9 +0,0 @@
-def test_mytoken_deploy(accounts, SalmonellaAttackToken):
-    initial_supply = 1000000 
-    deployer = accounts[0]
-    mytoken = SalmonellaAttackToken.deploy(initial_supply, {'from': deployer})
-
-    assert mytoken.totalSupply() == initial_supply * 10**18
-    assert mytoken.balanceOf(deployer) == initial_supply * 10**18
-    assert mytoken.name() == "SalmonellaAttackToken"
-    assert mytoken.symbol() == "SAT"
