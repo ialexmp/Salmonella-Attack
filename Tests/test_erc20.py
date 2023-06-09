475138db9c3218cc39e8edfbd3b614a81e9b3d25
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-09 10:23:18 +0200

    Update test_erc20.py
    addapt it to salmonella

--- Tests/test_erc20.py
+++ Tests/test_erc20.py
@@ -1,9 +1,9 @@
-def test_mytoken_deploy(accounts, MyToken):
+def test_mytoken_deploy(accounts, SalmonellaAttackToken):
     initial_supply = 1000000 
     deployer = accounts[0]
-    mytoken = MyToken.deploy(initial_supply, {'from': deployer})
+    mytoken = SalmonellaAttackToken.deploy(initial_supply, {'from': deployer})
 
     assert mytoken.totalSupply() == initial_supply * 10**18
     assert mytoken.balanceOf(deployer) == initial_supply * 10**18
-    assert mytoken.name() == "MyToken"
-    assert mytoken.symbol() == "MTK"
+    assert mytoken.name() == "SalmonellaAttackToken"
+    assert mytoken.symbol() == "SAT"
