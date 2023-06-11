b8ef90a46730ad9802899104204c04ab6c124720
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-11 20:38:23 +0200

    Update and rename test_erc20-SA.py to test_salmonellaERC20_bait.py

---
+++ tests/test_salmonellaERC20_bait.py
@@ -0,0 +1,38 @@
+import pytest
+
+def test_transfer(SalmonellaAttackToken, accounts):
+    
+    initial_supply = 10000000
+    deployer = accounts[2]
+    # deployer = accounts[1]
+    recipient = accounts[1]
+    SA_Token = SalmonellaAttackToken.deploy(initial_supply,{'from':deployer})
+   
+    #print(f"Deployer's address: {deployer.address}")
+    
+    assert SA_Token.totalSupply() == initial_supply * 10**18
+    assert SA_Token.balanceOf(deployer) == initial_supply * 10**18
+    assert SA_Token.name() == "SalmonellaAttackToken"
+    assert SA_Token.symbol() == "SAT"
+    
+    # Get the initial balances
+    owner_balance = SA_Token.balanceOf(deployer)
+    recipient_balance = SA_Token.balanceOf(recipient)
+    print("\n |---------------------- TEST RESULTS: ----------------------| ") 
+    print("\n Balances before Transaction: \n") 
+    print("   Sender:", owner_balance / 10**18, " (eth)" )
+    print("   Recipient:", recipient_balance / 10**18, " (eth)")
+
+    # Perform the transfer
+    amount = 100 
+    ethAmount = amount * 10**18
+    SA_Token.transfer(recipient, ethAmount, {'from': deployer})
+
+    # Get the updated balances
+    owner_balance = SA_Token.balanceOf(deployer)
+    recipient_balance = SA_Token.balanceOf(recipient)
+
+    print("\n Balances after Transaction: \n")
+    print("   Sender:", owner_balance / 10**18, " (eth)")
+    print("   Recipient:", recipient_balance / 10**18, " (eth)\n")
+    print("|----------------------------------------------------------| \n") 
