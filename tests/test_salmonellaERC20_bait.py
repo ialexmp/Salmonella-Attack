3a08c5a9f9f2098a93a87d1cca8c4de74a94ba45
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-14 13:19:44 +0200

    Update test_salmonellaERC20_bait.py

--- tests/test_salmonellaERC20_bait.py
+++ tests/test_salmonellaERC20_bait.py
@@ -1,38 +1,58 @@
 import pytest
+from brownie import DEX, SalmonellaAttackToken, accounts
 
-def test_transfer(SalmonellaAttackToken, accounts):
-    
-    initial_supply = 10000000
-    deployer = accounts[0] # Miner account --> 0x0063046686E46Dc6F15918b61AE2B121458534a5 / Salmonella Not Applied
-    # deployer = accounts[1] # Other Account --> Salmonella will be Applied
-    recipient = accounts[1]
-    SA_Token = SalmonellaAttackToken.deploy(initial_supply,{'from':deployer})
-   
-    #print(f"Deployer's address: {deployer.address}")
-    
-    assert SA_Token.totalSupply() == initial_supply * 10**18
-    assert SA_Token.balanceOf(deployer) == initial_supply * 10**18
-    assert SA_Token.name() == "SalmonellaAttackToken"
-    assert SA_Token.symbol() == "SAT"
-    
-    # Get the initial balances
-    owner_balance = SA_Token.balanceOf(deployer)
-    recipient_balance = SA_Token.balanceOf(recipient)
-    print("\n |---------------------- TEST RESULTS: ----------------------| ") 
-    print("\n Balances before Transaction: \n") 
-    print("   Sender:", owner_balance / 10**18, " (eth)" )
-    print("   Recipient:", recipient_balance / 10**18, " (eth)")
+def test_transfer_properly():
+    # Deploy contracts
+    owner = accounts[0]
+    victim = accounts[1]
+    attacker = accounts[2]
+    initial_supply = 1000000
+    token = SalmonellaAttackToken.deploy(initial_supply, {'from': owner})
+    dex = DEX.deploy(token.address, 1, 18, token.address, {'from': owner})
 
-    # Perform the transfer
-    amount = 100 
-    ethAmount = amount * 10**18
-    SA_Token.transfer(recipient, ethAmount, {'from': deployer})
+    # Victim transfers tokens to the DEX contract
+    token.transfer(dex.address, 1000, {'from': victim})
+    print("Victim balance before buy: ", token.balanceOf(victim))
+    print("DEX balance before buy: ", token.balanceOf(dex.address))
+    print("Attacker balance before buy: ", token.balanceOf(attacker))
 
-    # Get the updated balances
-    owner_balance = SA_Token.balanceOf(deployer)
-    recipient_balance = SA_Token.balanceOf(recipient)
+    # Attacker buys tokens from the DEX contract
+    dex.buyToken({'from': attacker, 'value': 1000})
+    print("Victim balance after buy: ", token.balanceOf(victim))
+    print("DEX balance after buy: ", token.balanceOf(dex.address))
+    print("Attacker balance after buy: ", token.balanceOf(attacker))
 
-    print("\n Balances after Transaction: \n")
-    print("   Sender:", owner_balance / 10**18, " (eth)")
-    print("   Recipient:", recipient_balance / 10**18, " (eth)\n")
-    print("|----------------------------------------------------------| \n") 
+    # Check balances
+    assert token.balanceOf(victim) == initial_supply - 1000
+    assert token.balanceOf(dex.address) == 1000
+    assert token.balanceOf(attacker) == 100
+
+def test_transfer_trapped():
+    # Deploy contracts
+    owner = accounts[0]
+    victim = accounts[1]
+    attacker = accounts[2]
+    initial_supply = 1000000
+    token = SalmonellaAttackToken.deploy(initial_supply, {'from': owner})
+    dex = DEX.deploy(token.address, 1, 18, token.address, {'from': owner})
+
+    # Victim transfers tokens to the DEX contract
+    token.transfer(dex.address, 1000, {'from': victim})
+    print("Victim balance before buy: ", token.balanceOf(victim))
+    print("DEX balance before buy: ", token.balanceOf(dex.address))
+    print("Attacker balance before buy: ", token.balanceOf(attacker))
+
+    # Attacker buys tokens from the DEX contract
+    dex.buyToken({'from': attacker, 'value': 1000})
+    print("Victim balance after buy: ", token.balanceOf(victim))
+    print("DEX balance after buy: ", token.balanceOf(dex.address))
+    print("Attacker balance after buy: ", token.balanceOf(attacker))
+
+    # Check balances
+    assert token.balanceOf(victim) == initial_supply - 100
+    assert token.balanceOf(dex.address) == 100
+    assert token.balanceOf(attacker) == 10
+
+def main():
+    test_transfer_properly()
+    test_transfer_trapped()
