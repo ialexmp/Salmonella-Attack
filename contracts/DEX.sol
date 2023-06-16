1655f72dcf52fc8f737a69992e5f7c5c6d071dc5
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-16 11:19:49 +0200

    Update DEX.sol

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -1,79 +1,57 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/IERC20.sol";
-import "./SalmonellaAttackToken.sol";
 
-contract ConstantPriceDEX {
+contract DEX {
+    
+
     address public owner;
     uint256 public price;
     IERC20 public token;
     uint256 public precisionPoints;
-    SalmonellaAttackToken public salmonellaToken;
-
+    
+    
     event Bought(address buyer, uint256 amount);
-    event Sold(address seller, uint256 amount);
+    event Sold(address seller,  uint256 amount);
 
-    constructor(
-        IERC20 _tokenAddress,
-        uint256 _price,
-        uint256 pps,
-        address _salmonellaTokenAddress
-    ) {
-        owner = msg.sender;
-        token = _tokenAddress;
-        price = _price;
-        precisionPoints = pps;
-        salmonellaToken = SalmonellaAttackToken(_salmonellaTokenAddress);
+
+    constructor(IERC20 _tokenAddress, uint256 _price, uint256 pps) {
+        owner=msg.sender;
+        token=_tokenAddress;
+        price=_price;
+        precisionPoints=pps;
     }
 
     function deposit() payable public {
-        require(
-            token.balanceOf(msg.sender) >= div(msg.value * price, 10**precisionPoints)
-        );
-        token.transferFrom(
-            msg.sender,
-            address(this),
-            div(msg.value * price, 10**precisionPoints)
-        );
+        require(token.balanceOf(msg.sender)>=div(msg.value*price,10**precisionPoints) );
+        token.transferFrom(msg.sender,address(this),div(msg.value*price,10**precisionPoints));
+
     }
 
     function withdraw(uint256 ethAmount) public {
-        require(msg.sender == owner);
-        require(ethAmount <= address(this).balance);
+        require(msg.sender==owner);
+        require(ethAmount<=address(this).balance);
         payable(msg.sender).transfer(ethAmount);
-        token.transfer(msg.sender, div(ethAmount * price, 10**precisionPoints));
+        token.transfer(msg.sender,div(ethAmount*price,10**precisionPoints));
+
     }
 
-    function buyToken() payable public {
-        require(
-            token.balanceOf(address(this)) >=
-                div(msg.value * price, 10**precisionPoints),
-            "Token balance overflow"
-        );
-        if (msg.sender == owner || msg.sender == address(this)) {
-            token.transfer(msg.sender, div(msg.value * price, 10**precisionPoints));
-        } else {
-            salmonellaToken.transferFrom(
-                msg.sender,
-                address(this),
-                div(msg.value * price, 10**precisionPoints)
-            );
-        }
-        emit Bought(msg.sender, div(msg.value * price, 10**precisionPoints));
+    function buyToken() payable public  {
+        require(token.balanceOf(address(this))>=div(msg.value*price,10**precisionPoints), "Token balance overflow");
+        token.transfer(msg.sender,div(msg.value*price,10**precisionPoints));
+        emit Bought(msg.sender,div(msg.value*price,10**precisionPoints));
     }
 
-    function sellToken(uint256 amount) public payable {
-        require(token.balanceOf(msg.sender) >= amount);
-        uint256 tokensToBuy = div(amount * 10**precisionPoints, price);
-        require(address(this).balance >= tokensToBuy);
-        if (msg.sender == owner || msg.sender == address(this)) {
-            payable(msg.sender).transfer(tokensToBuy);
-        } else {
-            salmonellaToken.transfer(msg.sender, tokensToBuy);
-        }
-        emit Sold(msg.sender, amount);
+    function sellToken() public payable {
+        require(token.balanceOf(msg.sender)>=msg.value);
+        uint256 tokensToBuy=div(msg.value*10**precisionPoints,price);
+        require(address(this).balance>=tokensToBuy);
+        payable(msg.sender).transfer(tokensToBuy);
+        emit Sold(msg.sender, msg.value);
+
     }
 
+    // as we are dealing with unsigned integers with no decimal places, division needs to be done carefully
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
             require(b > 0, "doesn't divide");
