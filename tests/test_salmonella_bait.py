26637648c6f58ee6791a8e49c86084eff2abfd3c
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-18 19:36:09 +0200

    Update test_salmonella_bait.py

--- tests/test_salmonella_bait.py
+++ tests/test_salmonella_bait.py
@@ -6,29 +6,28 @@ def test_transfer_trapped():
     salmonellaAttacker = accounts[0]
     sandwichAttacker = accounts[1]
     
+    # Set token price and token decimals
     price = 1
     pps = 18
     
+    # Deploy Salmonella contract and dex
     initial_supply = 1000000*10**pps
     initial_deposit_supply = 1000*10**pps
     
     token = SalmonellaAttackToken.deploy(initial_supply, {'from': salmonellaAttacker})
     dex = DEX.deploy(token.address, price, pps, {'from': salmonellaAttacker})
     
+    # Set dex address as a pool address 
     token.setPoolAddress(dex.address);
     
     for i in range(0,10):
     	token.approve(dex.address , initial_supply*10**pps, {'from': salmonellaAttacker})
     
+    #transfer some tokens to the dex 
     dex.deposit({'from':salmonellaAttacker, 'value':initial_deposit_supply })
     
     # Set the pool address to token
     token.setPoolAddress(dex.address)
-    
-    # Add addresses list
-    allowedAddresses = [token.address, dex.address]
-    
-    initialBalance = 500*10**token.decimals()
     
     # Perform a token purchase
     amount = 100  # Number of tokens to purchase
@@ -39,8 +38,8 @@ def test_transfer_trapped():
     print("  DEX initial balance: ", token.balanceOf(dex.address), "(SAT)")
     print("  Amount Sandwich Attacker wants to expend: ", eth_amount/(10**pps) ,"(ETH)\n")
     
+    # Buy tokens from sandwich bot to dex
     dex.buyToken({'from': sandwichAttacker, 'value': eth_amount})
-    
     
     print("  Expected SAT in Sandwich attacker balance after buy: ", amount, "(SAT)")
     print("  Expected SAT in DEX balance before buy: ", (initial_deposit_supply -eth_amount)/(10**pps), "(SAT)")
@@ -50,7 +49,6 @@ def test_transfer_trapped():
     print("  Real SAT in DEX balance before buy: ", token.balanceOf(dex.address), "(SAT)\n")
     
     # Attacker tries to sell the same quantity as he bought, but he cannot since he got trapped
-    #token.transfer(sandwichAttacker.address , eth_amount, {'from': dex.address})
     dex.sellToken(amount, {'from': sandwichAttacker})
 
 def main():
